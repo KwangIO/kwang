5bb8090ea973c8b2f4e12592eb03aaaae5165ac1
Quang Luong <quangio@outlook.com>
2018-11-04 01:45:19 +0800

    bugfix

--- src/linuxMain/kotlin/kwang/SampleLinux.kt
+++ src/linuxMain/kotlin/kwang/SampleLinux.kt
@@ -8,7 +8,9 @@ import kwang.backend.lwan.ServerLwan
 class SampleHandler: KwangHandler("/") {
     override fun handleGet(request: RequestContext, response: ResponseContext): UInt {
         println("Auth: ${request.authorization}")
-        response.setHeaders(listOf(Pair("haha", "abc"))).respond("123")
+        response
+            .setHeaders(listOf(Pair("meaning-of-life", "42"), Pair("looking-for", "job")))
+            .respond("123", "application/json")
         return 200u
     }
 }
